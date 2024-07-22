7fdb6ff8cd92c7b899176ff05fd2e2b7b8507eba
RobTov <tovelrob25@gmail.com>
2024-07-22 17:04:20 -0400

    added contact info component, ui changes

--- src/app/components/contact-info/contact-info.component.ts
+++ src/app/components/contact-info/contact-info.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { LanguageService } from '../../services/language.service';
 
 @Component({
   selector: 'app-contact-info',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule],
   templateUrl: './contact-info.component.html',
   styleUrl: './contact-info.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ContactInfoComponent { }
+export class ContactInfoComponent {
+  public languageService = inject(LanguageService);
+}
